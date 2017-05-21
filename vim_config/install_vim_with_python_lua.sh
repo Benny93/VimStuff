5e91401a6a29fd69c220dd7fea64d8f41ed28ecc
Benny93 <b.vollmer@student.uni-tuebingen.de>
2017-05-21 13:16:11 -0700

    Added option to build vim with python2

--- vim_config/install_vim_with_python_lua.sh
+++ vim_config/install_vim_with_python_lua.sh
@@ -13,11 +13,22 @@ apt-get remove -y vim-tiny vim-common vim-gui-common vim-nox
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
+#python3
+#./configure --with-features=huge \
+#	--enable-multibyte \
+#	--enable-rubyinterp=yes \
+#	--enable-python3interp=yes \
+#	--with-python3-config-dir=/usr/lib/python3.5/config \
+#	--enable-perlinterp=yes \
+#	--enable-luainterp=yes \
+#	--enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+#python2.7
 ./configure --with-features=huge \
 	--enable-multibyte \
 	--enable-rubyinterp=yes \
-	--enable-python3interp=yes \
-	--with-python3-config-dir=/usr/lib/python3.5/config \
+ 	--enable-pythoninterp=yes \
+ 	--with-python-config-dir=/usr/lib/python2.7/config \
 	--enable-perlinterp=yes \
 	--enable-luainterp=yes \
 	--enable-gui=gtk2 --enable-cscope --prefix=/usr
